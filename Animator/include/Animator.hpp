c5c3fc6cb4144a62a3892fb561666a7eb4ad64fc
Mikhail Mryasov <damaghack@gmail.com>
2023-05-20 06:12:28 +0700

    comment functions in Animator

--- Animator/include/Animator.hpp
+++ Animator/include/Animator.hpp
@@ -10,16 +10,38 @@ namespace anim
     class Animator
     {
     public:
+        /// @brief Constructor <Empty>
         Animator();
         ~Animator();
 
+        /// @brief Add animation in container, if container was empty set animation at begin
+        /// @param name - Key
+        /// @param animation - Value
+        /// @return current animation
         Animation* addAnimation(std::string name, Animation* animation);
+
+        /// @brief set animation at Key
+        /// @param name - Key
+        /// @return current animation
         Animation* setAnimation(std::string name);
+
+        /// @brief return current animation
+        /// @return current animation
         Animation* getAnimation();
+
+        /// @brief get animation at Key
+        /// @param name - Key
+        /// @return animation at Key
         Animation* getAnimation(std::string name);
 
+        /// @brief for Debug info
         void debug();
+
+        /// @brief manual cleanup
         void cleanup();
+
+        /// @brief return current frame of current animation
+        /// @return Frame
         Frame* getFrame();
     private:
         Map storage;
